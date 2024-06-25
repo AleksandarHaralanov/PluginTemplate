89f6e8a2e54dc1060774742da85aec492d57aa72
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-26 01:42:13 +0300

    Add better loggers and variables to grab the plugin's name and version from plugin.yml. Also improved comments

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import me.beezle.extras.UpdateUtil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class Main extends JavaPlugin {
+
+    private static String currentVersion;
+    private static String pluginName;
+
+    @Override
+    public void onEnable() {
+        PluginDescriptionFile pdf = this.getDescription();
+        currentVersion = pdf.getVersion();
+        pluginName = pdf.getName();
+
+        getLogger().info(String.format("[%s] v%s Enabled.", pluginName, currentVersion));
+
+        // Initialization logic (e.g., register listeners, commands)
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(String.format("[%s] v%s Disabled.", pluginName, currentVersion));
+
+        // Cleanup logic (e.g., save data, unregister listeners)
+    }
+}
