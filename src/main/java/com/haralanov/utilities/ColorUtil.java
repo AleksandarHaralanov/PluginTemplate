a81c3ff5ace01665613888e8915084046131d479
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-26 06:07:29 +0300

    Change parameter names

--- src/main/java/com/haralanov/utilities/ColorUtil.java
+++ src/main/java/com/haralanov/utilities/ColorUtil.java
@@ -5,14 +5,14 @@ public class ColorUtil {
     /**
      * Translates alternate color codes in the given text to Minecraft color codes.
      *
-     * @param colorChar The character used to denote color codes '&'.
-     * @param textToColorize The text containing the alternate color codes.
+     * @param altColorChar The character used to denote color codes '&'.
+     * @param textToTranslate The text containing the alternate color codes.
      * @return The text with the alternate color codes replaced by Minecraft color codes.
      */
-    public static String translateAlternateColorCodes(char colorChar, String textToColorize) {
-        char[] charArray = textToColorize.toCharArray();
+    public static String translateAlternateColorCodes(char altColorChar, String textToTranslate) {
+        char[] charArray = textToTranslate.toCharArray();
         for (int i = 0; i < charArray.length - 1; i++) {
-            if (charArray[i] == colorChar && "0123456789AaBbCcDdEeFf".indexOf(charArray[i + 1]) > -1) {
+            if (charArray[i] == altColorChar && "0123456789AaBbCcDdEeFf".indexOf(charArray[i + 1]) > -1) {
                 charArray[i] = '\u00A7';
                 charArray[i + 1] = Character.toLowerCase(charArray[i + 1]);
             }
